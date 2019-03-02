dd8a7cf8ef18474e77b76a6d35bb05daacc13ee8
Smilga <smilga@Smilgas-iMac.local>
2019-03-01 16:10:29 -0800

    Book Mini Project

--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import ReactDom from "react-dom";
 
 //ES6, Nested Components and React Tools
 
-function Greeting() {
+function Books() {
   return (
     <section>
-      <Person />
-      <Message />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </section>
   );
 }
-const Person = () => <h2>john doe</h2>;
-const Message = () => <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <CoverImage />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const CoverImage = () => (
+  <img
+    width="200"
+    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
+    alt="wonkey donkey"
+  />
+);
+const Title = () => <h1>The Wonkey Donkey</h1>;
+const Author = () => <p>by Craig Smith</p>;
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<Books />, document.getElementById("root"));
